b38c97f18d258aa762b62f41bfa40690a8614897
Christopher Biscardi <biscarch@sketcht.com>
2016-01-15 16:12:06 -0800

    basics

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 module.exports = {
-
+  parser: "babel-eslint",
   env: {
-    es6: true
+    browser: true,
+    es6: true,
+    node: true
+  },
+  ecmaFeatures: {
+    modules: true,
+  },
+  rules: {
+    strict: 0,
+    quotes: [2, 'single', 'avoid-escape']
   }
-
 };
